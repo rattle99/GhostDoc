2c41b4f62a7c6aab1106da830bdd772622f59251
Alok Jain <alokj702@gmail.com>
2024-06-23 13:28:38 +0530

    Integration, Added changes from different branches

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ from tika import parser as tika_parser
 import requests  # Add for making HTTP requests
 from presidio_analyzer import AnalyzerEngine
 analyzer = AnalyzerEngine()
+import importlib
+import CustomFaker
 
 app = Flask(__name__)
+
+mapDict = {}
 UPLOAD_FOLDER = './uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 final_result_set = []
@@ -33,7 +37,7 @@ def chunk_text_into_400_words(text):
     
     if current_chunk:
         chunks.append(" ".join(current_chunk))
-
+    
     return chunks
 
 # Function to call ML Model 1 and get JSON output
@@ -42,6 +46,7 @@ def call_pretrained_model(text_chunks):
     URL = 'http://localhost:5000/pii'
     payload = {'text': text_chunks}
     result = requests.post(URL, json=payload, verify=False)
+    
     modelResponse = result.json()
     return modelResponse
 
@@ -61,16 +66,37 @@ def call_precedio_model(text_chunks):
 
 
 # Function to compare outputs of two models
-def concatenate_model_outputs(output1, output2):
-
+def model_outputs(output1, output2):
+    result_set=[]
     for entity1 in output1["response"]:
-        final_result_set.append(entity1)
+        result_set.append(entity1)
 
     for entity2 in output2["response"]:
-        final_result_set.append(entity2)
+        result_set.append(entity2)
 
-    # print(final_result_set)
+    return result_set
 
+def transform_chunks(result,chunk):
+    module_name = "CustomFaker"
+    module = importlib.import_module(module_name)
+    
+    for json_object in result:
+        start_index= json_object['start']
+        end_index=json_object['end']
+        method_name = json_object['entity_group']
+        replaced_text = chunk[start_index:end_index+1]
+        res=''
+        if replaced_text in mapDict:
+            res = mapDict[replaced_text]
+        else:
+            method = getattr(module, method_name)
+            res =method()
+            mapDict[replaced_text]=  res
+
+        chunk = chunk[:start_index] + str(res) + chunk[end_index:]
+        
+    
+    return chunk
 
 # Route for uploading file and processing
 @app.route('/upload', methods=['POST'])
@@ -83,27 +109,29 @@ def upload_file():
         return jsonify({'error': 'No selected file'})
 
     filename = secure_filename(file.filename)
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(file_path)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+    
 
     # # Extract text using Tika
     # file_path = "C:\\Users\\smate\\Desktop\\story.txt"
-    text = extract_text_using_tika(file_path)
-
-    os.remove(file_path)  # Remove the uploaded file to clean up
+    text = extract_text_using_tika(filepath)
+    os.remove(filepath)  # Remove the uploaded file to clean up
 
     # Chunk text into 400-word segments
     chunks = chunk_text_into_400_words(text)
-
+   # print(chunks)
+    final_result_set=[]
+    modified_chunks_list=[]
     for chunk in chunks:
         # Call ML Model 1 and ML Model 2
         model1_output = call_pretrained_model(chunk)
         model2_output = call_precedio_model(chunk)
         # print(model2_output)
-        concatenate_model_outputs(model1_output, model2_output)
-
-    print(final_result_set)
-    return {"response" : "new response"}
+        result_set=model_outputs(model1_output, model2_output)
+        modified_chunks_list.append(transform_chunks(result_set,chunk))
+    return ''.join(modified_chunks_list)
+    
 
 # Route for homepage
 @app.route('/')
